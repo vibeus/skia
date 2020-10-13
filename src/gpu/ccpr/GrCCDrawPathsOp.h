5ca6b40517bbcb634f91d0a404476c5fcbcc6003
Jiulong Wang <jiulongw@vibe.us>
2020-10-13 15:09:09 -0700

    Fix GrCCPerFlushResources error caused by cache eviction
    By storing the cached atlas proxies in GrCCDrawPathsOp. This is
    important because GrCCPathCache may evict an entry that is being used
    in current flush, causing the op to lose the proxy it intended to draw
    from.
    
    Co-authored with Chris Dalton <csmartdalton@google.com>
    
    Bug: chromium:1102117
    Change-Id: I2e4b9360a84732269b6ce98f4d8adfc7e7b9735c
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/326576
    Reviewed-by: Chris Dalton <csmartdalton@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>

--- src/gpu/ccpr/GrCCDrawPathsOp.h
+++ src/gpu/ccpr/GrCCDrawPathsOp.h
@@ -120,10 +120,10 @@ private:
         SkPMColor4f fColor;
 
         GrCCPathCache::OnFlushEntryRef fCacheEntry;
+        sk_sp<GrTextureProxy> fCachedAtlasProxy;
+        GrCCAtlas::CoverageType fCachedAtlasCoverageType;
         SkIVector fCachedMaskShift;
-        bool fDoCopyToA8Coverage = false;
         bool fDoCachePathMask = false;
-        SkDEBUGCODE(bool fWasCountedAsRender = false);
 
         SingleDraw* fNext = nullptr;
 
